5180b2a2979525036a31df5aa9c9494a9c26bcde
vti <viacheslav.t@gmail.com>
2012-08-18 10:52:40 +0300

    Updated version

--- lib/Protocol/WebSocket.pm
+++ lib/Protocol/WebSocket.pm
@@ -3,7 +3,7 @@ package Protocol::WebSocket;
 use strict;
 use warnings;
 
-our $VERSION = 0.00906;
+our $VERSION = 0.10;
 
 1;
 __END__
@@ -33,7 +33,7 @@ Protocol::WebSocket - WebSocket protocol
                 $hs->build_frame(
                     type    => 'close',
                     version => $version
-                )
+                )->to_bytes
             );
 
             return;
@@ -52,7 +52,7 @@ L<Protocol::WebSocket> supports the following WebSocket protocol versions:
 
     draft-ietf-hybi-17 (latest)
     draft-ietf-hybi-10
-    draft-ietf-hybi-00
+    draft-ietf-hybi-00 (with HAProxy support)
     draft-hixie-75
 
 By default the latest version is used. The WebSocket version is detected
@@ -109,7 +109,7 @@ Viacheslav Tykhanovskyi, C<vti@cpan.org>.
 
 =head1 COPYRIGHT
 
-Copyright (C) 2010-2011, Viacheslav Tykhanovskyi.
+Copyright (C) 2010-2012, Viacheslav Tykhanovskyi.
 
 This program is free software, you can redistribute it and/or modify it under
 the same terms as Perl 5.10.
