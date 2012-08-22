a5655ea8a316d195d5fe809c564e0359910f54e8
vti <viacheslav.t@gmail.com>
2012-08-22 16:58:31 +0300

    Updated version and Changes

--- lib/Protocol/WebSocket.pm
+++ lib/Protocol/WebSocket.pm
@@ -3,7 +3,7 @@ package Protocol::WebSocket;
 use strict;
 use warnings;
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 use Protocol::WebSocket::Frame;
 use Protocol::WebSocket::Handshake::Client;
