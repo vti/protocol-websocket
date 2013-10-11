653c5a830247ad1c6db737af884e97c20340bc39
vti <viacheslav.t@gmail.com>
2013-10-11 10:06:19 +0300

    Credits

--- lib/Protocol/WebSocket.pm
+++ lib/Protocol/WebSocket.pm
@@ -116,13 +116,15 @@ Matthew Lien (BlueT)
 
 Joao Orui
 
+Toshio Ito (debug-ito)
+
 =head1 AUTHOR
 
 Viacheslav Tykhanovskyi, C<vti@cpan.org>.
 
 =head1 COPYRIGHT
 
-Copyright (C) 2010-2012, Viacheslav Tykhanovskyi.
+Copyright (C) 2010-2013, Viacheslav Tykhanovskyi.
 
 This program is free software, you can redistribute it and/or modify it under
 the same terms as Perl 5.10.
