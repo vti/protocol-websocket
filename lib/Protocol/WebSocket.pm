0e541226b239138bde4a5f0c899c62889ee96f75
vti <viacheslav.t@gmail.com>
2014-01-07 22:34:10 +0200

    Checking in changes prior to tagging of version 0.16.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index cf7ea3b..c3489aa 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for perl module Protocol::WebSocket
    
     {{$NEXT}}
    
    +0.16 2014-01-07T20:33:59Z
    +
    + - just meta files update
    +
     0.15 2013-11-03T15:54:47Z
    
      - Explicitly stringify $payload in _mask() method (Toshio Ito)

--- lib/Protocol/WebSocket.pm
+++ lib/Protocol/WebSocket.pm
@@ -3,7 +3,7 @@ package Protocol::WebSocket;
 use strict;
 use warnings;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 use Protocol::WebSocket::Frame;
 use Protocol::WebSocket::Handshake::Client;
