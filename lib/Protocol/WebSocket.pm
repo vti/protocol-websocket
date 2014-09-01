ed82a980fe4fafcb09cbca715c856110897f3bc5
vti <viacheslav.t@gmail.com>
2014-09-01 17:45:23 +0300

    Checking in changes prior to tagging of version 0.18.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e57496b..30018d5 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for perl module Protocol::WebSocket
    
     {{$NEXT}}
    
    +0.18 2014-09-01T14:45:16Z
    +
    + - Digest::SHA1 -> Digest::SHA (Michal Špaček)
    +
     0.17 2014-04-09T08:12:01Z
    
      - perl 5.8 unpack fix (Graham Ollis)

--- lib/Protocol/WebSocket.pm
+++ lib/Protocol/WebSocket.pm
@@ -3,7 +3,7 @@ package Protocol::WebSocket;
 use strict;
 use warnings;
 
-our $VERSION = '0.17';
+our $VERSION = '0.18';
 
 use Protocol::WebSocket::Frame;
 use Protocol::WebSocket::Handshake::Client;
