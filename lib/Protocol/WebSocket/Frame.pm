60fcecb7906f772fb1a3344665f052d41f1dfbdb
vti <viacheslav.t@gmail.com>
2017-12-12 20:15:48 +0100

    Revert "Always UTF-8 encode the buffer of text frames; never encode others"
    This reverts commit 54b0cde4bb3ce58f52c62bcee81409ea70e5dedf.

--- lib/Protocol/WebSocket/Frame.pm
+++ lib/Protocol/WebSocket/Frame.pm
@@ -43,9 +43,12 @@ sub new {
         $self->opcode($TYPES{$self->{type}});
     }
 
-    $self->{buffer} = $self->is_text ?
-        Encode::encode('UTF-8', $buffer) :
-        $buffer;
+    if (Encode::is_utf8($buffer)) {
+        $self->{buffer} = Encode::encode('UTF-8', $buffer);
+    }
+    else {
+        $self->{buffer} = $buffer;
+    }
 
     $self->{version} ||= 'draft-ietf-hybi-17';
 
@@ -358,10 +361,8 @@ L<Math::Random::Secure> is installed it is used instead.
     Protocol::WebSocket::Frame->new('data');   # same as (buffer => 'data')
     Protocol::WebSocket::Frame->new(buffer => 'data', type => 'close');
 
-Create a new L<Protocol::WebSocket::Frame> instance. When creating a C<text>
-frame (the default behaviour if C<type> is not specified) the string data is
-encoded into UTF-8 bytes; for other frame types it is assumed to already be in
-bytes.
+Create a new L<Protocol::WebSocket::Frame> instance. Automatically detect if the
+passed data is a Perl string (UTF-8 flag) or bytes.
 
 When called with more than one arguments, it takes the following named arguments
 (all of them are optional).
