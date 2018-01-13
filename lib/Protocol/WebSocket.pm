d78fcba3273c577dba27f91aec1137cf43de2a57
vti <viacheslav.t@gmail.com>
2018-01-13 10:57:01 +0100

    update credits & copy

--- lib/Protocol/WebSocket.pm
+++ lib/Protocol/WebSocket.pm
@@ -140,13 +140,15 @@ Graham Ollis
 
 Anton Petrusevich
 
+Eric Wastl
+
 =head1 AUTHOR
 
 Viacheslav Tykhanovskyi, C<vti@cpan.org>.
 
 =head1 COPYRIGHT
 
-Copyright (C) 2010-2017, Viacheslav Tykhanovskyi.
+Copyright (C) 2010-2018, Viacheslav Tykhanovskyi.
 
 This program is free software, you can redistribute it and/or modify it under
 the same terms as Perl 5.10.
