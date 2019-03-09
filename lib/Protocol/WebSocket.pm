3da830194c279998d3595fa6d624ea58ffecc2b1
vti <viacheslav.t@gmail.com>
2019-03-09 10:35:22 +0100

    Checking in changes prior to tagging of version 0.26.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 8760ae9..b993177 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for perl module Protocol::WebSocket
    
     {{$NEXT}}
    
    +0.26 2019-03-09T09:35:15Z
    +
      - Introduce MAX_FRAGMENTS_AMOUNT to change the allowable amount of fragments (Daniel Kamil Kozar)
      - Update draft-ietf-hybi-00 tests to include the error text in response line (Daniel Kamil Kozar)

--- lib/Protocol/WebSocket.pm
+++ lib/Protocol/WebSocket.pm
@@ -3,7 +3,7 @@ package Protocol::WebSocket;
 use strict;
 use warnings;
 
-our $VERSION = '0.25';
+our $VERSION = '0.26';
 
 use Protocol::WebSocket::Frame;
 use Protocol::WebSocket::Handshake::Client;
