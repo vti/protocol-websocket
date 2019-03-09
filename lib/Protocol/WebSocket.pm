4aeec177820585cd9f5707d7bae8ff4edefa0fde
vti <viacheslav.t@gmail.com>
2019-03-09 10:31:51 +0100

    Checking in changes prior to tagging of version 0.25.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b1b75a9..f5ffefe 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,8 @@ Revision history for perl module Protocol::WebSocket
    
     {{$NEXT}}
    
    +0.25 2019-03-09T09:31:45Z
    +
      - The "Wrong response line" error now includes the actual response line
    
     0.24 2018-01-13T09:59:07Z

--- lib/Protocol/WebSocket.pm
+++ lib/Protocol/WebSocket.pm
@@ -3,7 +3,7 @@ package Protocol::WebSocket;
 use strict;
 use warnings;
 
-our $VERSION = '0.24';
+our $VERSION = '0.25';
 
 use Protocol::WebSocket::Frame;
 use Protocol::WebSocket::Handshake::Client;
